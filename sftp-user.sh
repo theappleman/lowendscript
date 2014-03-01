2289ecab6056b5ae64a2b2e4430b774279896e49
Daniel Cordero <theappleman@gmail.com>
2014-03-01 11:42:26 +0000

    Update sftp-user.sh

--- sftp-user.sh
+++ sftp-user.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 USER=${1?Need user}
+ROOT=${2:-/var/www/vhosts}
 
 set -e
 
-mkdir -p /var/www/$USER
+mkdir -p $ROOT/$USER
 
-useradd -d /var/www/$USER -g www-data -G sftponly $USER
-passwd $USER
+useradd -d $ROOT/$USER -g www-data -G sftponly $USER
+
+mkdir -p $ROOT/$USER/{htdocs,private}
+chown $USER:www-data $ROOT/$USER/{htdocs,private}
+
+PASSWORD=$(pwgen -s 31)
+
+echo "$USER:$PASSWORD" | chpasswd
+
+
