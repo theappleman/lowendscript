689e6bc26032da4162661397cbc7dde2e098c1a9
apple <apple@applehq.eu>
2014-04-06 12:20:39 +0100

    convert spaces to tabs

--- setup-debian.sh
+++ setup-debian.sh
@@ -1,90 +1,90 @@
 #!/bin/bash
 
 function check_install {
-    if [ -z "`which "$1" 2>/dev/null`" ]
-    then
-        executable=$1
-        shift
-        while [ -n "$1" ]
-        do
-            DEBIAN_FRONTEND=noninteractive apt-get -q -y install "$1"
-            print_info "$1 installed for $executable"
-            shift
-        done
-    else
-        print_warn "$2 already installed"
-    fi
+	if [ -z "`which "$1" 2>/dev/null`" ]
+	then
+		executable=$1
+		shift
+		while [ -n "$1" ]
+		do
+			DEBIAN_FRONTEND=noninteractive apt-get -q -y install "$1"
+			print_info "$1 installed for $executable"
+			shift
+		done
+	else
+		print_warn "$2 already installed"
+	fi
 }
 
 function check_remove {
-    if [ -n "`which "$1" 2>/dev/null`" ]
-    then
-        DEBIAN_FRONTEND=noninteractive apt-get -q -y remove --purge "$2"
-        print_info "$2 removed"
-    else
-        print_warn "$2 is not installed"
-    fi
+	if [ -n "`which "$1" 2>/dev/null`" ]
+	then
+		DEBIAN_FRONTEND=noninteractive apt-get -q -y remove --purge "$2"
+		print_info "$2 removed"
+	else
+		print_warn "$2 is not installed"
+	fi
 }
 
 function check_sanity {
-    # Do some sanity checking.
-    if [ $(/usr/bin/id -u) != "0" ]
-    then
-        die 'Must be run by root user'
-    fi
+	# Do some sanity checking.
+	if [ $(/usr/bin/id -u) != "0" ]
+	then
+		die 'Must be run by root user'
+	fi
 
-    if [ ! -f /etc/debian_version ]
-    then
-        die "Distribution is not supported"
-    fi
+	if [ ! -f /etc/debian_version ]
+	then
+		die "Distribution is not supported"
+	fi
 }
 
 function die {
-    echo "ERROR: $1" > /dev/null 1>&2
-    exit 1
+	echo "ERROR: $1" > /dev/null 1>&2
+	exit 1
 }
 
 function get_domain_name() {
-    # Getting rid of the lowest part.
-    domain=${1%.*}
-    lowest=`expr "$domain" : '.*\.\([a-z][a-z]*\)'`
-    case "$lowest" in
-    com|net|org|gov|edu|co)
-        domain=${domain%.*}
-        ;;
-    esac
-    lowest=`expr "$domain" : '.*\.\([a-z][a-z]*\)'`
-    [ -z "$lowest" ] && echo "$domain" || echo "$lowest"
+	# Getting rid of the lowest part.
+	domain=${1%.*}
+	lowest=`expr "$domain" : '.*\.\([a-z][a-z]*\)'`
+	case "$lowest" in
+	com|net|org|gov|edu|co)
+		domain=${domain%.*}
+		;;
+	esac
+	lowest=`expr "$domain" : '.*\.\([a-z][a-z]*\)'`
+	[ -z "$lowest" ] && echo "$domain" || echo "$lowest"
 }
 
 function get_password() {
-    # Check whether our local salt is present.
-    SALT=/var/lib/radom_salt
-    if [ ! -f "$SALT" ]
-    then
-        head -c 512 /dev/urandom > "$SALT"
-        chmod 400 "$SALT"
-    fi
-    password=`(cat "$SALT"; echo $1) | md5sum | base64`
-    echo ${password:0:13}
+	# Check whether our local salt is present.
+	SALT=/var/lib/radom_salt
+	if [ ! -f "$SALT" ]
+	then
+		head -c 512 /dev/urandom > "$SALT"
+		chmod 400 "$SALT"
+	fi
+	password=`(cat "$SALT"; echo $1) | md5sum | base64`
+	echo ${password:0:13}
 }
 
 function install_dash {
-    check_install dash dash
-    rm -f /bin/sh
-    ln -s dash /bin/sh
+	check_install dash dash
+	rm -f /bin/sh
+	ln -s dash /bin/sh
 }
 
 function install_mysql {
-    # Install the MySQL packages
-    check_install mysqld mysql-server
-    check_install mysql mysql-client
+	# Install the MySQL packages
+	check_install mysqld mysql-server
+	check_install mysql mysql-client
 
-    # Install a low-end copy of the my.cnf to disable InnoDB, and then delete
-    # all the related files.
-    invoke-rc.d mysql stop
-    rm -f /var/lib/mysql/ib*
-    cat > /etc/mysql/conf.d/lowendbox.cnf <<END
+	# Install a low-end copy of the my.cnf to disable InnoDB, and then delete
+	# all the related files.
+	invoke-rc.d mysql stop
+	rm -f /var/lib/mysql/ib*
+	cat > /etc/mysql/conf.d/lowendbox.cnf <<END
 [mysqld]
 key_buffer_size = 256K
 query_cache_size = 0
@@ -93,69 +93,69 @@ max_connections = 20
 max_user_connections = 10
 innodb_buffer_pool_size=5M
 END
-    invoke-rc.d mysql start
+	invoke-rc.d mysql start
 
-    # Generating a new password for the root user.
-    passwd=`get_password root@mysql`
-    mysqladmin password "$passwd"
-    cat > ~/.my.cnf <<END
+	# Generating a new password for the root user.
+	passwd=`get_password root@mysql`
+	mysqladmin password "$passwd"
+	cat > ~/.my.cnf <<END
 [client]
 user = root
 password = $passwd
 END
-    chmod 600 ~/.my.cnf
+	chmod 600 ~/.my.cnf
 }
 
 function install_nginx {
-    check_install nginx nginx
-    
-    # Need to increase the bucket size for Debian 5.
-    cat > /etc/nginx/conf.d/lowendbox.conf <<END
+	check_install nginx nginx
+
+	# Need to increase the bucket size for Debian 5.
+	cat > /etc/nginx/conf.d/lowendbox.conf <<END
 server_names_hash_bucket_size 64;
 END
 
-    invoke-rc.d nginx restart
+	invoke-rc.d nginx restart
 }
 
 function install_php {
-    check_install php5-fpm php5-fpm php5-mysql
+	check_install php5-fpm php5-fpm php5-mysql
 }
 
 function install_polipo {
-    check_install polipo polipo
-    cat > /etc/polipo/config <<END
+	check_install polipo polipo
+	cat > /etc/polipo/config <<END
 chunkHighMark = 819200
 objectHighMark = 128
 censoredHeaders = set-cookie, cookie, cookie2, from, accept-language
 censorReferer = true
 END
-    service polipo restart
+	service polipo restart
 }
 
 function install_syslogd {
-    # We just need a simple vanilla syslogd. Also there is no need to log to
-    # so many files (waste of fd). Just dump them into
-    # /var/log/(cron/mail/messages)
-    check_install /usr/sbin/syslogd inetutils-syslogd
-    invoke-rc.d inetutils-syslogd stop
+	# We just need a simple vanilla syslogd. Also there is no need to log to
+	# so many files (waste of fd). Just dump them into
+	# /var/log/(cron/mail/messages)
+	check_install /usr/sbin/syslogd inetutils-syslogd
+	invoke-rc.d inetutils-syslogd stop
 
-    for file in /var/log/*.log /var/log/mail.* /var/log/debug /var/log/syslog
-    do
-        [ -f "$file" ] && rm -f "$file"
-    done
-    for dir in fsck news
-    do
-        [ -d "/var/log/$dir" ] && rm -rf "/var/log/$dir"
-    done
+	for file in /var/log/*.log /var/log/mail.* /var/log/debug /var/log/syslog
+	do
+		[ -f "$file" ] && rm -f "$file"
+	done
+	for dir in fsck news
+	do
+		[ -d "/var/log/$dir" ] && rm -rf "/var/log/$dir"
+	done
 
-    cat > /etc/syslog.conf <<END
+	cat > /etc/syslog.conf <<END
 *.*;mail.none;cron.none -/var/log/messages
-cron.*                  -/var/log/cron
-mail.*                  -/var/log/mail
+cron.*				  -/var/log/cron
+mail.*				  -/var/log/mail
 END
 
-    [ -d /etc/logrotate.d ] || mkdir -p /etc/logrotate.d
-    cat > /etc/logrotate.d/inetutils-syslogd <<END
+	[ -d /etc/logrotate.d ] || mkdir -p /etc/logrotate.d
+	cat > /etc/logrotate.d/inetutils-syslogd <<END
 /var/log/cron
 /var/log/mail
 /var/log/messages {
@@ -166,100 +166,100 @@ END
    compress
    sharedscripts
    postrotate
-      /etc/init.d/inetutils-syslogd reload >/dev/null
+	  /etc/init.d/inetutils-syslogd reload >/dev/null
    endscript
 }
 END
 
-    invoke-rc.d inetutils-syslogd start
+	invoke-rc.d inetutils-syslogd start
 }
 
 function install_wordpress {
-    check_install wget wget
-    if [ -z "$1" ]
-    then
-        die "Usage: `basename $0` wordpress <hostname>"
-    fi
+	check_install wget wget
+	if [ -z "$1" ]
+	then
+		die "Usage: `basename $0` wordpress <hostname>"
+	fi
 
-    # Downloading the WordPress' latest and greatest distribution.
-    mkdir /tmp/wordpress.$$
-    wget -O - http://wordpress.org/latest.tar.gz | \
-        tar zxf - -C /tmp/wordpress.$$
-    mv /tmp/wordpress.$$/wordpress "/var/www/$1"
-    rm -rf /tmp/wordpress.$$
-    chown root:root -R "/var/www/$1"
+	# Downloading the WordPress' latest and greatest distribution.
+	mkdir /tmp/wordpress.$$
+	wget -O - http://wordpress.org/latest.tar.gz | \
+		tar zxf - -C /tmp/wordpress.$$
+	mv /tmp/wordpress.$$/wordpress "/var/www/$1"
+	rm -rf /tmp/wordpress.$$
+	chown root:root -R "/var/www/$1"
 
-    # Setting up the MySQL database
-    dbname=`echo $1 | tr . _`
-    userid=`get_domain_name $1`
-    # MySQL userid cannot be more than 15 characters long
-    userid="${userid:0:15}"
-    passwd=`get_password "$userid@mysql"`
-    cp "/var/www/$1/wp-config-sample.php" "/var/www/$1/wp-config.php"
-    sed -i "s/database_name_here/$dbname/; s/username_here/$userid/; s/password_here/$passwd/" \
-        "/var/www/$1/wp-config.php"
-    mysqladmin create "$dbname"
-    echo "GRANT ALL PRIVILEGES ON \`$dbname\`.* TO \`$userid\`@localhost IDENTIFIED BY '$passwd';" | \
-        mysql
+	# Setting up the MySQL database
+	dbname=`echo $1 | tr . _`
+	userid=`get_domain_name $1`
+	# MySQL userid cannot be more than 15 characters long
+	userid="${userid:0:15}"
+	passwd=`get_password "$userid@mysql"`
+	cp "/var/www/$1/wp-config-sample.php" "/var/www/$1/wp-config.php"
+	sed -i "s/database_name_here/$dbname/; s/username_here/$userid/; s/password_here/$passwd/" \
+		"/var/www/$1/wp-config.php"
+	mysqladmin create "$dbname"
+	echo "GRANT ALL PRIVILEGES ON \`$dbname\`.* TO \`$userid\`@localhost IDENTIFIED BY '$passwd';" | \
+		mysql
 
-    # Setting up Nginx mapping
-    cat > "/etc/nginx/sites-enabled/$1.conf" <<END
+	# Setting up Nginx mapping
+	cat > "/etc/nginx/sites-enabled/$1.conf" <<END
 server {
-    server_name $1;
-    root /var/www/$1;
-    include /etc/nginx/fastcgi_php;
-    location / {
-        index index.php;
-        if (!-e \$request_filename) {
-            rewrite ^(.*)$  /index.php last;
-        }
-    }
+	server_name $1;
+	root /var/www/$1;
+	include /etc/nginx/fastcgi_php;
+	location / {
+		index index.php;
+		if (!-e \$request_filename) {
+			rewrite ^(.*)$  /index.php last;
+		}
+	}
 }
 END
-    invoke-rc.d nginx reload
+	invoke-rc.d nginx reload
 }
 
 function print_info {
-    echo -n -e '\e[1;36m'
-    echo -n $1
-    echo -e '\e[0m'
+	echo -n -e '\e[1;36m'
+	echo -n $1
+	echo -e '\e[0m'
 }
 
 function print_warn {
-    echo -n -e '\e[1;33m'
-    echo -n $1
-    echo -e '\e[0m'
+	echo -n -e '\e[1;33m'
+	echo -n $1
+	echo -e '\e[0m'
 }
 
 function remove_unneeded {
-    # Some Debian have portmap installed. We don't need that.
-    check_remove /sbin/portmap portmap
+	# Some Debian have portmap installed. We don't need that.
+	check_remove /sbin/portmap portmap
 
-    # Remove rsyslogd, which allocates ~30MB privvmpages on an OpenVZ system,
-    # which might make some low-end VPS inoperatable. We will do this even
-    # before running apt-get update.
-    check_remove /usr/sbin/rsyslogd rsyslog
+	# Remove rsyslogd, which allocates ~30MB privvmpages on an OpenVZ system,
+	# which might make some low-end VPS inoperatable. We will do this even
+	# before running apt-get update.
+	check_remove /usr/sbin/rsyslogd rsyslog
 
-    # Other packages that seem to be pretty common in standard OpenVZ
-    # templates.
-    check_remove /usr/sbin/apache2 'apache2*'
-    check_remove /usr/sbin/named bind9
-    check_remove /usr/sbin/smbd 'samba*'
-    check_remove /usr/sbin/nscd nscd
+	# Other packages that seem to be pretty common in standard OpenVZ
+	# templates.
+	check_remove /usr/sbin/apache2 'apache2*'
+	check_remove /usr/sbin/named bind9
+	check_remove /usr/sbin/smbd 'samba*'
+	check_remove /usr/sbin/nscd nscd
 
-    # Need to stop sendmail as removing the package does not seem to stop it.
-    if [ -f /usr/lib/sm.bin/smtpd ]
-    then
-        invoke-rc.d sendmail stop
-        check_remove /usr/lib/sm.bin/smtpd 'sendmail*'
-    fi
+	# Need to stop sendmail as removing the package does not seem to stop it.
+	if [ -f /usr/lib/sm.bin/smtpd ]
+	then
+		invoke-rc.d sendmail stop
+		check_remove /usr/lib/sm.bin/smtpd 'sendmail*'
+	fi
 }
 
 function update_upgrade {
-    # Run through the apt-get update/upgrade first. This should be done before
-    # we try to install any package
-    apt-get -q -y update
-    apt-get -q -y upgrade
+	# Run through the apt-get update/upgrade first. This should be done before
+	# we try to install any package
+	apt-get -q -y update
+	apt-get -q -y upgrade
 }
 
 ########################################################################
@@ -270,32 +270,32 @@ export PATH=/bin:/usr/bin:/sbin:/usr/sbin
 check_sanity
 case "$1" in
 mysql)
-    install_mysql
-    ;;
+	install_mysql
+	;;
 nginx)
-    install_nginx
-    ;;
+	install_nginx
+	;;
 php)
-    install_php
-    ;;
+	install_php
+	;;
 polipo)
-    install_polipo
-    ;;
+	install_polipo
+	;;
 system)
-    remove_unneeded
-    update_upgrade
-    install_dash
-    install_syslogd
-    ;;
+	remove_unneeded
+	update_upgrade
+	install_dash
+	install_syslogd
+	;;
 wordpress)
-    install_wordpress $2
-    ;;
+	install_wordpress $2
+	;;
 *)
-    echo 'Usage:' `basename $0` '[option]'
-    echo 'Available option:'
-    for option in system mysql nginx php polipo wordpress
-    do
-        echo '  -' $option
-    done
-    ;;
+	echo 'Usage:' `basename $0` '[option]'
+	echo 'Available option:'
+	for option in system mysql nginx php polipo wordpress
+	do
+		echo '  -' $option
+	done
+	;;
 esac
