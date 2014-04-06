e177566d2937da7af379bac7ee2328bb3d49c4ec
Daniel Cordero <github@xxoo.ws>
2014-04-06 11:01:56 +0000

    Update setup-debian.sh

--- setup-debian.sh
+++ setup-debian.sh
@@ -119,18 +119,6 @@ END
 
 function install_php {
     check_install php5-fpm php5-fpm php5-mysql
-
-    cat > /etc/nginx/fastcgi_php <<END
-location ~ \.php$ {
-    include /etc/nginx/fastcgi_params;
-
-    fastcgi_index index.php;
-    fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-    if (-f \$request_filename) {
-        fastcgi_pass unix:/var/run/php5-fpm.sock;
-    }
-}
-END
 }
 
 function install_polipo {
